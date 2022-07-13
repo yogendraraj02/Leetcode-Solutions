e5b94c81f9ebe9ab86f39ff649fca3548adc4236
Yogendra Raj <83568306+yogendraraj02@users.noreply.github.com>
2022-07-13 19:53:49 +0530

    Time: 1 ms (99.98%), Space: 48.7 MB (22.37%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -3,11 +3,17 @@ class Solution {
         int s = 0;
         int e = nums.length-1;
         while(s < e){
-            if(nums[s]%2==1 && nums[e] % 2 == 0){
+            if((nums[s] & 1) == 1 && (nums[e] & 1) == 0){
                 swap(nums,s,e);
+                s++;
+                e--;
             }
-            if(nums[s]% 2 == 0) s++;
-            if(nums[e] % 2!=0)  e--;
+            if((nums[s] & 1) == 0){
+                s++;
+            }
+            if((nums[e] & 1) == 1){
+                e--;
+            }
         }
         return nums;
     }
