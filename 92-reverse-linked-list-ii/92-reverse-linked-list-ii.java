5ed4f5a2b9cbd8d87528b35beb454622716eb500
Yogendra Raj <83568306+yogendraraj02@users.noreply.github.com>
2022-07-21 17:48:33 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (37.90%) - LeetHub

---
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(right - left == 0)   return head;
+        ListNode prev = null , curr = head;
+        for(int i=0; i < left - 1 && curr!= null ; i++){
+            prev = curr;
+            curr = curr.next;
+            
+        }
+        ListNode lefttail = prev, revLast = curr;
+        //reverse
+        int l = 0;
+        prev = curr;
+        curr = curr.next;
+        ListNode forw = null;
+        while(l++ < right - left && curr!= null){
+            forw = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = forw;
+        }
+        
+       
+       if(lefttail == null){
+           head = prev;
+       }else {
+           lefttail.next = prev;         // 1 -> 4
+        }
+            
+        revLast.next = forw;          //rightHead;
+        return head;
+    }
+}
+
+
+// ListNode rightHead = forw;
+//System.out.println("cr:"+curr.val +" lt:"+lefttail.val +" revLast:"+revLast.val+" frow:"+forw.val);
+        
