ce3806e3af1bf1fcd4f31d286601c60dbbdb9618
Yogendra Raj <83568306+yogendraraj02@users.noreply.github.com>
2022-07-21 16:11:13 +0530

    Time: 5 ms (75.47%), Space: 43.5 MB (65.79%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())    return false;
+        int[] freq = new int[26];
+        for(char ch : s.toCharArray()){
+            freq[ch - 'a']++;
+        }
+        for(char ch : t.toCharArray()){
+            freq[ch - 'a']--;
+        }
+        for(int i=0; i < 26;i++){
+            if(freq[i] != 0){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
