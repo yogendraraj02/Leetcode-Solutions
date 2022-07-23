25237db8a9447456d644724c9e07989080c824a3
Yogendra Raj <83568306+yogendraraj02@users.noreply.github.com>
2022-07-23 13:44:26 +0530

    Time: 85 ms (45.36%), Space: 109.8 MB (37.85%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.java
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -5,16 +5,14 @@ class Solution {
         for(int winend =0; winend < fruits.length ; winend++){
             map.put(fruits[winend],map.getOrDefault(fruits[winend],0)+1);
             
-            while(winstart < fruits.length && map.size() > 2){
+            while(winstart < winend && map.size() > 2){
                 map.put(fruits[winstart],map.get(fruits[winstart])-1);
                 if(map.get(fruits[winstart]) == 0){
                     map.remove(fruits[winstart]);
                 }
                 winstart++;
             }
-            if(map.size() <= 2){
-                ans = Math.max(winend - winstart + 1, ans);
-            }
+            ans = Math.max(winend - winstart + 1, ans);
         }
         return ans;
     }
