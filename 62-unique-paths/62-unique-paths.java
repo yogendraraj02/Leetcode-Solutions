7b89e2f3551b78cf45ebe52ebc355d50ed118e32
Yogendra Raj <83568306+yogendraraj02@users.noreply.github.com>
2022-08-01 12:32:35 +0530

    Time: 1 ms (55.39%), Space: 40.4 MB (82.16%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,17 +1,39 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        return getPaths(0,0,m-1,n-1,dp);
-        
+        //return getPathsMemo(0,0,m-1,n-1,dp);
+        return getPathsTab(dp , m,n);
     }
-    public int getPaths(int i ,int j ,int m,int n,int[][]dp){
+    public int getPathsMemo(int i ,int j ,int m,int n,int[][]dp){
         if(i > m || j > n )  return 0;  
         if(dp[i][j] !=0)    return dp[i][j];
         if(i == m && j == n){
             return 1;
         }
-        
-        dp[i][j] = getPaths(i+1,j,m,n,dp) + getPaths(i,j+1,m,n,dp);
+                
+        dp[i][j] = getPathsMemo(i+1,j,m,n,dp) + getPathsMemo(i,j+1,m,n,dp);
         return dp[i][j];
     }
+    public int getPathsTab(int[][]dp,int m,int n){
+        
+        //fill the dp in bottom row (everyone will be one)
+        for(int i = m - 1, j = 0; j < n; j++){
+            dp[i][j] = 1;
+        }
+        //fill the dp in last col (everyone will be one)
+        for(int i = 0, j =  n -1; i < m ; i++){
+            dp[i][j] = 1;
+        }
+        // now the remaining 
+        //formula = dp[i][j] = dp[i+1][j] + dp[i][j+1];
+        for(int i = m - 2; i >= 0 ; i--){
+            
+            for(int j = n - 2 ; j >= 0 ; j--){
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+            }
+        }
+        return dp[0][0];
+        
+    }
+    
 }
