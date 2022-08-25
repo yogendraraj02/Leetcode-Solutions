9afe510d27aa0f3b88ae301d72839c1d3087065e
Yogendra Raj <83568306+yogendraraj02@users.noreply.github.com>
2022-08-25 10:52:16 +0530

    Time: 4 ms (84.52%), Space: 46.2 MB (62.21%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -4,17 +4,17 @@ class Solution {
         
         int[] freq = new int[26];
         
-        for(char ch : r.toCharArray()){
+        for(char ch : m.toCharArray()){
             freq[ch - 'a']++;
         }
-        for(char ch : m.toCharArray()){
-            freq[ch -'a']--;
-        }
         for(char ch : r.toCharArray()){
-            if(freq[ch - 'a'] >= 1){
+            
+            if(freq[ch - 'a'] == 0){
                 return false;
             }
+            freq[ch -'a']--;
         }
+
         
         return true;
     }
