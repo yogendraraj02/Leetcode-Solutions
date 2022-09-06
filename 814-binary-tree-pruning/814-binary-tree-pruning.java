b4dde29a6dc5ef0bd71a13c0cf33a61c6acc01e6
Yogendra Raj <83568306+yogendraraj02@users.noreply.github.com>
2022-09-07 00:21:15 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (72.06%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.java
+++ 814-binary-tree-pruning/814-binary-tree-pruning.java
@@ -17,11 +17,7 @@ class Solution {
     public TreeNode pruneTree(TreeNode root) {
         
         if(root == null )   return null;
-        if(root.left == null && root.right == null){
-            if(root.val == 0){
-                return null;
-            }
-        }
+        
         
         TreeNode left = pruneTree(root.left);
         TreeNode right = pruneTree(root.right);
