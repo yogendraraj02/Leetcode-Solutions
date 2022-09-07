cf4efbadef76adfcd6734c3ce22845c26e1d35f2
Yogendra Raj <83568306+yogendraraj02@users.noreply.github.com>
2022-09-07 13:16:23 +0530

    Time: 98 ms (5.84%), Space: 66.4 MB (12.56%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
@@ -1,31 +1,27 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
+ * struct TreeNode {
  *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
 class Solution {
-    public String tree2str(TreeNode root) {
-        if(root == null) return "";
-        if(root.left == null && root.right == null){
-            return "" + root.val;
+public:
+    string tree2str(TreeNode* root) {
+        if(!root) return "";
+        if(!root ->left && !root -> right){
+            return to_string(root->val);
         }
-        int val = root.val;
-        String left = tree2str(root.left);
-        String  right = tree2str(root.right);
-        
+        string left = tree2str(root ->left);
+        string right = tree2str(root -> right);
         left = "(" + left + ")";
-        if(right != "")
-            right = "(" + right + ")";
-        return val + left + right;
+        if(right != ""){
+            right = "(" + right +")";
+        }
+        return to_string(root -> val) + left  + right;
     }
-}
+};
