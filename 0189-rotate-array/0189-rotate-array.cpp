e5c118e404c89ebb5772952963bda22d84db885d
Yogendra Raj <83568306+yogendraraj02@users.noreply.github.com>
2022-10-31 12:00:06 +0530

    Time: 91 ms (14.98%), Space: 25 MB (84.86%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,20 +4,9 @@ public:
         int n = nums.size();
         k = k % n;
         
-        int i = 0 , j = n - k - 1;
-        while(i < j){
-            swap(nums[i++],nums[j--]);
-        }
-        i = n - k;
-        j = n - 1;
-        while(i < j){
-            swap(nums[i++],nums[j--]);
-        }
-        i = 0;
-        j = n-1;
-        while(i < j){
-            swap(nums[i++],nums[j--]);
-        }
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin() + k ,nums.end());
         
     }
 };
