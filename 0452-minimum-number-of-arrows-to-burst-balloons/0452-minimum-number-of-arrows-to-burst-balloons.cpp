64ffb474e551025720d5e8e99157b7e6a43e07bc
Yogendra Raj <83568306+yogendraraj02@users.noreply.github.com>
2023-01-05 22:22:48 +0530

    Time: 1062 ms (27.99%), Space: 102.4 MB (5.07%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -5,11 +5,10 @@ public:
         sort(points.begin() , points.end());
         pair<int,int> prev;
         int cnt = 1;
-        vector<int> curr = points[0];
-        prev = make_pair(curr[0],curr[1]);
+        prev = make_pair(points[0][0],points[0][1]);
         
         for(int i=1; i < points.size() ; i++){
-            curr = points[i];
+            vector<int> curr = points[i];
             if(prev.second < curr[0]){
                 //independent
                 prev = {curr[0],curr[1]};
